ded9e55bbef8aa9c27245bdf9235dd83bd5bc40d
Antonio <67932072+AntonioBellito@users.noreply.github.com>
2020-07-30 11:35:18 -0500

    Actualizacion def learn
    Correciones de calculos en def learn

--- AgenteInteligente.py
+++ AgenteInteligente.py
@@ -55,12 +55,20 @@ class Qlearner(object):
         else:
             return np.random.choice([a for a in range(self.action_shape)])
         
-        
+    # CREACIÓN DE MÉTODO PARA APRENDER
+
+    # Se define la función que permitirá al agente poder añadir el conocimiento
+    # a esta función se le pasan los parametros 'self', 'obs', 'action', 'reward', 'next_obs'
+    # los cuales se definen en las funciones anteriores ya creadas 
     def learn(self, obs, action, reward, next_obs):
+        # Se inicializa una variable para almacenar un nuevo valor creado 
         discrete_obs = self.discretize(obs)
         discrete_next_obs = self.discretize(next_obs)
+         # Se inicializa una nueva variable la cual almacenará la recompenza obtenida hasta el momento 
         td_target = reward + self.gamma * np.max(self.Q[discrete_next_obs])
+        # Si se encuentra con algún error se guardará en una variable para indicar dónde se generó
         td_error = td_target - self.Q[discrete_obs][action]
+        # Se guardará la acción o el error
         self.Q[discrete_obs][action] += self.alpha*td_error
         
         
