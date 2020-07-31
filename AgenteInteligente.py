91c9b01bbf22ae0b07c63532da1b12313d48e6e1
Martin <mirandamaraten@gmail.com>
2020-07-30 19:06:18 -0500

    Update
    Ups

--- AgenteInteligente.py
+++ AgenteInteligente.py
@@ -94,24 +94,24 @@ class Qlearner(object):
         
  #METODO PARA ENTRENAR AL AGENTE
       
-def train(agent, environment):   # aqui definimos el metodo train en la cual tendremos un agente y un enrtorno que es enviroment
-       best_reward = -float('inf')   # definimos la mejor recompensa que sera menos float infinito 
-       for episode in range(MAX_NUM_EPISODES):   # definimos los episodios 
-        done  = False     #definimos la variable done por defecto es false
-        obs = environment.reset() #
-        total_reward = 0.0 # recompensa total acomulado en 0
-    while not done:   # definimos un bucle la cual mientras no hayamos finalizado la ejecusion 
-            action = agent.get_action(obs)   # accion elegida segun la ecuacion de Q-learning
-            next_obs, reward, done, info = environment.step(action)  # definimos los parametros y hara que el enviroment ejecute el .step
-            agent.learn(obs, action, reward, next_obs)   #con esta accion hacemos que el agente aprenda en base a la observacion
-            obs = next_obs   # esta es la siguiente observacion para ejecutar el siguiente paso
+def train(agent, environment): # aqui definimos el metodo train en la cual tendremos un agente y un enrtorno que es enviroment
+    best_reward = -float('inf') # definimos la mejor recompensa que sera menos float infinito 
+    for episode in range(MAX_NUM_EPISODES): # definimos los episodios 
+        done  = False #definimos la variable done por defecto es false
+        obs = environment.reset() # recompensa total acomulado en 0
+        total_reward = 0.0
+        while not done:  # definimos un bucle la cual mientras no hayamos finalizado la ejecusion
+            #environment.render()
+            action = agent.get_action(obs) # accion elegida segun la ecuacion de Q-learning
+            next_obs, reward, done, info = environment.step(action) # definimos los parametros y hara que el enviroment ejecute el .step
+            agent.learn(obs, action, reward, next_obs) #con esta accion hacemos que el agente aprenda en base a la observacion
+            obs = next_obs # esta es la siguiente observacion para ejecutar el siguiente paso
             total_reward += reward  # la recompensa se incrementa
-    if total_reward > best_reward: #fijamos si la recompensa total supera a la actual
-            best_reward = total_reward  # entonces sera recompensa anterior es igual a la recompensa actual.
-print("Episodio número {} con recompensa: {}, mejor recompensa: {}, epsilon: {}".format(episode, total_reward, best_reward, agent.epsilon)) #aqui imprimimos el episodio y el epsilon para ver con que valor termina
-        
+        if total_reward > best_reward: #fijamos si la recompensa total supera a la actual
+            best_reward = total_reward # entonces sera recompensa anterior es igual a la recompensa actual.
+        print("Episodio número {} con recompensa: {}, mejor recompensa: {}, epsilon: {}".format(episode, total_reward, best_reward, agent.epsilon)) #aqui imprimimos el episodio y el epsilon para ver con que valor termina
 ## de todas las politicas de entrenamiento que se han obtenido se devuelve laa mejor de todas
-return np.argmax(agent.Q, axis = 2) 
+        return np.argmax(agent.Q, axis = 2) 
 
 
 #queremos ser capaces de medir como ha ido todo lo que ha aprendido sometiendolo a una prueba, un test
